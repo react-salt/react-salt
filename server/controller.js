7d80fa58572b3a37ab48c50d6fa845f39be1694f
yanweiqing <yanweiqing@youzan.com>
2016-03-02 10:08:54 +0800

    预备添加组件列表
    todo:
    目前examples的数据直接传递到前端界面，这种方案如果实现则是最佳方案，比较稳妥的是在模板层先实现多个examples，但是目前看来有一些风险。

--- server/controller.js
+++ server/controller.js
@@ -1,10 +1,14 @@
 import Meow from 'koa-meow';
 
 import render from './middlewares/render';
+import renderCp from './middlewares/renderCp';
 
 export default class Controller extends Meow.Controller {
   render(dir) {
     render.apply(this.ctx, arguments);
+  }
+  renderCp(dir) {
+  	renderCp.apply(this.ctx, arguments);
   }
 }
 
--- server/middlewares/renderCp.js
+++ server/middlewares/renderCp.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import config from 'config';
 import path from 'path';
 
-let tmpl = _.template(fs.readFileSync(path.join(config.path.layout, 'default.html')));
+let tmpl = _.template(fs.readFileSync(path.join(config.path.layout, 'component.html')));
 
 export default function render(param) {
   this.body = tmpl(param);
