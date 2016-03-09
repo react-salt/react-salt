57e9c6f3aa52042c01224afdeb6bcbe84c979b15
yanweiqing <yanweiqing@youzan.com>
2016-03-09 09:40:42 +0800

    解决less加载bug 初步完成demo界面
    todo: 新版的salt-ui需要投入使用

--- app/pages/component.js
+++ app/pages/component.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import PageLayout from '../core/page-layout.js';
-import Ex from '../components/example.js';
+import Demo from '../components/example.js';
+
 
 let Examples = [];
 
 MYM.list.map((item) => {
 	let name = item.substring(0, item.length - 4);
-	let Ex = name[0].toUpperCase() + name.slice(1);
 	let Example= require('../../node_modules/rs-' + MYM.cp + '/examples/' + name + '.jsx');
-    console.log(Example.default);
 	Examples.push(Example.default);
 });
 
 export default class Page extends PageLayout {
-    renderMainExtra() {
+    renderMainContent() {
         let { readme, code } = MYM;
+
         return (
-			<div>
-				<div dangerouslySetInnerHTML={{__html: readme}} />
+			<div className="col-sm-9">
+				<div dangerouslySetInnerHTML={{__html: readme}} className="wrapper-marked" />
+                <h2>代码DEMO<i className="glyphicon glyphicon-leaf" /></h2>
 				{
 					Examples.map((Example, index) => {
-						return (
-							<div key={index}>
+                        return (
+                            <Demo key={index} code={code[index]}>
 								<Example />
-								<pre dangerouslySetInnerHTML={{__html: code[index]}}/>
-							</div>
+							</Demo>
 						);
 					})
 				}
